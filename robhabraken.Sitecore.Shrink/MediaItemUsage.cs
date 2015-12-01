673647e2bc1b310978904185f331401e7460337e
Rob Habraken <roberthabraken@hotmail.com>
2015-12-02 00:31:25 +0100

    Initial commit of media usage and publishing helper classes to start gathering stats

---
+++ robhabraken.Sitecore.Shrink/MediaItemUsage.cs
@@ -0,0 +1,117 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Data.Archiving;
+
+namespace robhabraken.Sitecore.Shrink
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MediaItemUsage
+    {
+        private const string MEDIA_FOLDER_ID = "{FE5DD826-48C6-436D-B87A-7C4210C7413B}";
+
+        public void Scan()
+        {
+            this.UsedItemCount = 0;
+            this.PublishedItemCount = 0;
+            this.UnusedItems = new List<Item>();
+            this.UnpublishedItems = new List<Item>();
+
+            //TODO: check if type is a good choice (could be item list as well)
+            this.OldVersions = new List<Version>();
+
+            var database = Factory.GetDatabase("master");
+            var archive = ArchiveManager.GetArchive("archive", database);
+            var publishingHelper = new PublishingHelper();
+
+            var root = database.Items["/sitecore/media library"];
+            var descendants = root.Axes.GetDescendants();
+            foreach (var item in descendants)
+            {
+                if (!item.Template.ID.ToString().Equals(workinprogress.MEDIA_FOLDER_ID))
+                {
+                    // count all items that are actually media items (not folders)
+                    this.MediaItemCount++;
+
+                    // update and get referrers
+                    Globals.LinkDatabase.UpdateReferences(item);
+                    var itemReferrers = Globals.LinkDatabase.GetReferrers(item);
+
+                    // find all valid referrers
+                    var used = false;
+                    foreach (var itemLink in itemReferrers)
+                    {
+                        if (itemLink != null)
+                        {
+                            var referencedItem = itemLink.GetSourceItem();
+                            if (referencedItem != null)
+                            {
+                                used = true;
+                            }
+                        }
+                    }
+
+                    // process usage results
+                    if (used)
+                    {
+                        this.UsedItemCount++;
+                    }
+                    else
+                    {
+                        this.UnusedItems.Add(item);
+                    }
+
+                    // count targets that this item is actually published to and process results
+                    if (publishingHelper.ListPublishedTargets(item).Count > 0)
+                    {
+                        this.PublishedItemCount++;
+                    }
+                    else
+                    {
+                        this.UnpublishedItems.Add(item);
+                    }
+
+                    //TODO: find versions that are old and or obsolete
+                }
+            }
+        }
+
+        public int MediaItemCount { get; set; }
+
+        public int UsedItemCount { get; set; }
+
+        public int UnusedItemCount
+        {
+            get
+            {
+                return this.MediaItemCount - this.UsedItemCount;
+            }
+        }
+
+        public List<Item> UnusedItems { get; set; }
+
+        public int PublishedItemCount { get; set; }
+
+        public int UnpublishedItemCount
+        {
+            get
+            {
+                return this.MediaItemCount - this.PublishedItemCount;
+            }
+        }
+
+        public List<Item> UnpublishedItems { get; set;  }
+
+        //TODO: use this (or just count size of OldVersions list?
+        public int OldVersionsItemCount { get; set; }
+
+        //TODO: fill this list
+        public List<Version> OldVersions { get; set; }
+
+    }
+}
---
+++ robhabraken.Sitecore.Shrink/PublishingHelper.cs
@@ -0,0 +1,153 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Publishing;
+
+namespace robhabraken.Sitecore.Shrink
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Helper methods for easy publishing from code, that are not natively available in the Sitecore API.
+    /// </summary>
+    public class PublishingHelper
+    {
+        public enum PublishState
+        {
+            Published,
+            Changed,
+            NotPublished
+        }
+
+        /// <summary>
+        /// Publishes an item in all languages to all publishing targets.
+        /// </summary>
+        /// <param name="item">The item to publish</param>
+        /// <param name="deep">If deep is set to true, all publishable descendants will be published as well</param>
+        /// <param name="compareRevisions">If compareRevisions is set to true, the Revision field in the versions of the item in source and target database are compared. If the Revision fields are the same, the item will not be published (also known as Smart Publish)</param>
+        public void PublishAll(Item item, bool deep, bool compareRevisions)
+        {
+            var targets = this.GetPublishingTargets(item);
+            if (targets.Count > 0)
+            {
+                PublishManager.PublishItem(item, targets.ToArray(), item.Languages, deep, compareRevisions);
+            }
+        }
+
+        /// <summary>
+        /// Publishes an item in all languages to all publishing targets, while also publishing ancestors that are not yet published and thus are blocking this item from being published.
+        /// This could be useful if a content editor creates a new folder in the media library for example, uploads a new file into that folder and publishes the file, but forgets to publish the folder itself (a common mistake when new to Sitecore).
+        /// Since there would be no reason at all a content editor wants to publish a certain item, but doesn't want it to be shown by not publishing the parent folder, it's safe to assume this is what the content editor actually wants to happen.
+        /// 
+        /// This function doesn't publish descendants (subitems) because that would not only cause an infinite loop, but that would, in theory, publish the whole item tree.
+        /// Therefore, this function only works to publish a single item, guaranteeing it will be visible in all publishing targets. However, publish restrictions are respected by this function, so be aware you use those restrictions correctly. 
+        /// </summary>
+        /// <param name="item">The item to publish, including its ancestors</param>
+        public void PublishReversedRecursive(Item item)
+        {
+            if (item != null && !PresentInAllPublishingTargets(item))
+            {
+                this.PublishReversedRecursive(item.Parent);
+                this.PublishAll(item, false, false);
+            }
+        }
+
+        /// <summary>
+        /// Verifies if the given item is present in all publishing targets.
+        /// </summary>
+        /// <param name="item">The item to look for in the publishing targets</param>
+        /// <returns>True if the item is present in all publishing targets (and thus published), false if the item isn't present in one or more publishing targets</returns>
+        public bool PresentInAllPublishingTargets(Item item)
+        {
+            bool published = true;
+
+            var publishingTargets = this.GetPublishingTargets(item);
+            foreach (var database in publishingTargets)
+            {
+                published &= database.SelectSingleItem(item.ID.ToString()) != null;
+            }
+
+            return published;
+        }
+
+        /// <summary>
+        /// Returns a list of publishing targets that the given item is actually published to.
+        /// </summary>
+        /// <param name="item">The item to look for in the publishing targets</param>
+        /// <returns>A list of all publishing targets where the given item is found</returns>
+        public List<Database> ListPublishedTargets(Item item)
+        {
+            var publishedTargets = new List<Database>();
+
+            var publishingTargets = this.GetPublishingTargets(item);
+            foreach (var database in publishingTargets)
+            {
+                if (database.SelectSingleItem(item.ID.ToString()) != null)
+                {
+                    publishedTargets.Add(database);
+                }
+            }
+
+            return publishedTargets;
+        }
+
+        /// <summary>
+        /// Returns a list of all publishing targets, accompanied with the publishing state of the current item 
+        /// </summary>
+        /// <param name="item">The item to look for in the publishing targets</param>
+        /// <returns>A dictionary of all publishing targets with their publishing state</returns>
+        public Dictionary<string, PublishState> ListTargets(Item item)
+        {
+            var targets = new Dictionary<string, PublishState>();
+
+            var publishingTargets = this.GetPublishingTargets(item);
+            foreach (var database in publishingTargets)
+            {
+                var state = PublishState.NotPublished;
+                var remoteItem = database.GetItem(item.ID, item.Language, item.Version);
+                if (remoteItem != null)
+                {
+                    if (item.Statistics.Revision.Equals(remoteItem.Statistics.Revision))
+                    {
+                        state = PublishState.Published;
+                    }
+                    else
+                    {
+                        state = PublishState.Changed;
+                    }
+                }
+                targets.Add(database.Name, state);
+            }
+
+            return targets;
+        }
+
+        /// <summary>
+        /// Returns all publishing targets as a List of Database objects.
+        /// This is actually the list to publish all items to, as it is incorrect to assume there is only one publishing target, called 'web'.
+        /// </summary>
+        /// <param name="item">The item to publish, used to determine the source database</param>
+        /// <returns>A list of publishing targets for the database of the given item</returns>
+        private List<Database> GetPublishingTargets(Item item)
+        {
+            var publishingTargets = new List<Database>();
+
+            var publishingTargetsItem = item.Database.GetItem("/sitecore/system/publishing targets");
+            if (publishingTargetsItem != null)
+            {
+                var children = publishingTargetsItem.GetChildren();
+                foreach (Item child in children)
+                {
+                    var targetDatabase = Factory.GetDatabase(child["target database"]);
+                    if (targetDatabase != null)
+                    {
+                        publishingTargets.Add(targetDatabase);
+                    }
+                }
+            }
+
+            return publishingTargets;
+        }
+
+    }
+}
