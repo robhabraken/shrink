c0e695425dde678cb7fbfc41904cd0d1e30366a1
Rob Habraken <roberthabraken@hotmail.com>
2015-12-05 12:05:18 +0100

    Test multiple files in multiple directories of different file types (PDF, png, jpg)

--- Website/index.aspx.cs
+++ Website/index.aspx.cs
@@ -15,10 +15,12 @@ namespace shrink
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var exampleItem = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{04DAD0FD-DB66-4070-881F-17264CA257E1}"));
+            var exampleItem1 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{04DAD0FD-DB66-4070-881F-17264CA257E1}"));
+            var exampleItem2 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{6AA5AA9F-071A-4808-91AC-709FAAFFB310}"));
+            var exampleItem3 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{9775973B-91D8-4FEF-A130-A774C61CA4AE}"));
 
             var tidyUp = new TidyUp();
-            tidyUp.Download(new List<Item>() { exampleItem }, "D:/");
+            tidyUp.Download(new List<Item>() { exampleItem1, exampleItem2, exampleItem3 }, "D:/");
         }
     }
 }
