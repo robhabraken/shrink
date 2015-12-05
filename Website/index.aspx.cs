81357583cf8395620fcf12d1fc0b6225bac2a2d4
Rob Habraken <roberthabraken@hotmail.com>
2015-12-05 15:39:52 +0100

    Update test case for deleting items

--- Website/index.aspx.cs
+++ Website/index.aspx.cs
@@ -18,11 +18,11 @@ namespace shrink
             var exampleItem1 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{04DAD0FD-DB66-4070-881F-17264CA257E1}"));
             var exampleItem2 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{6AA5AA9F-071A-4808-91AC-709FAAFFB310}"));
             var exampleItem3 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{9775973B-91D8-4FEF-A130-A774C61CA4AE}"));
-            var exampleItem4 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{8E158CC2-5F5D-426E-8897-F9573EBA976E}"));
+            var exampleItem4 = Factory.GetDatabase("master").GetItem(new Sitecore.Data.ID("{D07AFF79-10A3-4154-84C0-BEA2A2C538E3}"));
 
             var tidyUp = new TidyUp("master");
             //tidyUp.Download(new List<Item>() { exampleItem1, exampleItem2, exampleItem3 }, "D:/");
-            tidyUp.Recycle(new List<Item>() { exampleItem4 });
+            tidyUp.Delete(new List<Item>() { exampleItem4 });
         }
     }
 }
