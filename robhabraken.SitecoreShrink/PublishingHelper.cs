3a5af39a95abfcbf418b2323514f0474b214cdae
Rob Habraken <roberthabraken@hotmail.com>
2015-12-06 19:46:22 +0100

    Update comments of publishing helper to match new commenting style

--- robhabraken.SitecoreShrink/PublishingHelper.cs
+++ robhabraken.SitecoreShrink/PublishingHelper.cs
@@ -22,9 +22,9 @@ namespace robhabraken.SitecoreShrink
         /// <summary>
         /// Publishes an item in all languages to all publishing targets.
         /// </summary>
-        /// <param name="item">The item to publish</param>
-        /// <param name="deep">If deep is set to true, all publishable descendants will be published as well</param>
-        /// <param name="compareRevisions">If compareRevisions is set to true, the Revision field in the versions of the item in source and target database are compared. If the Revision fields are the same, the item will not be published (also known as Smart Publish)</param>
+        /// <param name="item">The item to publish.</param>
+        /// <param name="deep">If deep is set to true, all publishable descendants will be published as well.</param>
+        /// <param name="compareRevisions">If compareRevisions is set to true, the Revision field in the versions of the item in source and target database are compared. If the Revision fields are the same, the item will not be published (also known as Smart Publish).</param>
         public void PublishAll(Item item, bool deep, bool compareRevisions)
         {
             var targets = this.GetPublishingTargets(item);
@@ -36,13 +36,15 @@ namespace robhabraken.SitecoreShrink
 
         /// <summary>
         /// Publishes an item in all languages to all publishing targets, while also publishing ancestors that are not yet published and thus are blocking this item from being published.
+        /// </summary>
+        /// <remarks>
         /// This could be useful if a content editor creates a new folder in the media library for example, uploads a new file into that folder and publishes the file, but forgets to publish the folder itself (a common mistake when new to Sitecore).
         /// Since there would be no reason at all a content editor wants to publish a certain item, but doesn't want it to be shown by not publishing the parent folder, it's safe to assume this is what the content editor actually wants to happen.
         /// 
         /// This function doesn't publish descendants (subitems) because that would not only cause an infinite loop, but that would, in theory, publish the whole item tree.
         /// Therefore, this function only works to publish a single item, guaranteeing it will be visible in all publishing targets. However, publish restrictions are respected by this function, so be aware you use those restrictions correctly. 
-        /// </summary>
-        /// <param name="item">The item to publish, including its ancestors</param>
+        /// </remarks>
+        /// <param name="item">The item to publish, including its ancestors.</param>
         public void PublishReversedRecursive(Item item)
         {
             if (item != null && !PresentInAllPublishingTargets(item))
@@ -55,8 +57,8 @@ namespace robhabraken.SitecoreShrink
         /// <summary>
         /// Verifies if the given item is present in all publishing targets.
         /// </summary>
-        /// <param name="item">The item to look for in the publishing targets</param>
-        /// <returns>True if the item is present in all publishing targets (and thus published), false if the item isn't present in one or more publishing targets</returns>
+        /// <param name="item">The item to look for in the publishing targets.</param>
+        /// <returns>True if the item is present in all publishing targets (and thus published), false if the item isn't present in one or more publishing targets.</returns>
         public bool PresentInAllPublishingTargets(Item item)
         {
             bool published = true;
@@ -73,8 +75,8 @@ namespace robhabraken.SitecoreShrink
         /// <summary>
         /// Returns a list of publishing targets that the given item is actually published to.
         /// </summary>
-        /// <param name="item">The item to look for in the publishing targets</param>
-        /// <returns>A list of all publishing targets where the given item is found</returns>
+        /// <param name="item">The item to look for in the publishing targets.</param>
+        /// <returns>A list of all publishing targets where the given item is found.</returns>
         public List<Database> ListPublishedTargets(Item item)
         {
             var publishedTargets = new List<Database>();
@@ -92,10 +94,10 @@ namespace robhabraken.SitecoreShrink
         }
 
         /// <summary>
-        /// Returns a list of all publishing targets, accompanied with the publishing state of the current item 
+        /// Returns a list of all publishing targets, accompanied with the publishing state of the current item.
         /// </summary>
-        /// <param name="item">The item to look for in the publishing targets</param>
-        /// <returns>A dictionary of all publishing targets with their publishing state</returns>
+        /// <param name="item">The item to look for in the publishing targets.</param>
+        /// <returns>A dictionary of all publishing targets with their publishing state.</returns>
         public Dictionary<string, PublishState> ListTargets(Item item)
         {
             var targets = new Dictionary<string, PublishState>();
@@ -124,10 +126,12 @@ namespace robhabraken.SitecoreShrink
 
         /// <summary>
         /// Returns all publishing targets as a List of Database objects.
-        /// This is actually the list to publish all items to, as it is incorrect to assume there is only one publishing target, called 'web'.
         /// </summary>
-        /// <param name="item">The item to publish, used to determine the source database</param>
-        /// <returns>A list of publishing targets for the database of the given item</returns>
+        /// <remarks>
+        /// This is actually the list to publish all items to, as it is incorrect to assume there is only one publishing target, called 'web'.
+        /// </remarks>
+        /// <param name="item">The item to publish, used to determine the source database.</param>
+        /// <returns>A list of publishing targets for the database of the given item.</returns>
         private List<Database> GetPublishingTargets(Item item)
         {
             var publishingTargets = new List<Database>();
