b436d28c32be5de0daf558c1445d2f6822439738
Rob Habraken <roberthabraken@hotmail.com>
2015-12-07 01:36:27 +0100

    Add functionality to find items with multiple versions in the media item usage class

--- robhabraken.SitecoreShrink/MediaItemUsage.cs
+++ robhabraken.SitecoreShrink/MediaItemUsage.cs
@@ -3,6 +3,7 @@ namespace robhabraken.SitecoreShrink
 {
     using Sitecore;
     using Sitecore.Configuration;
+    using Sitecore.Data;
     using Sitecore.Data.Items;
     using System;
     using System.Collections.Generic;
@@ -14,17 +15,20 @@ namespace robhabraken.SitecoreShrink
     {
         public const string MEDIA_FOLDER_TEMPLATE_ID = "{FE5DD826-48C6-436D-B87A-7C4210C7413B}";
 
+        private Database database;
+
+        public MediaItemUsage(string databaseName)
+        {
+            this.database = Factory.GetDatabase(databaseName);
+        }
+
         public void Scan()
         {
             this.MediaItemCount = 0;
             this.UnusedItems = new List<Item>();
             this.UnpublishedItems = new List<Item>();
-
-            //TODO: check if type is a good choice (could be item list as well)
-            this.OldVersions = new List<Version>();
-
-            //TODO: make master database configurable?
-            var database = Factory.GetDatabase("master");
+            this.OldVersions = new List<Item>();
+            
             var publishingHelper = new PublishingHelper();
 
             var root = database.Items["/sitecore/media library"];
@@ -54,6 +58,7 @@ namespace robhabraken.SitecoreShrink
                         }
                     }
                     
+                    // add the item to the appropriate collections based on its state (used, published or multiple versions)
                     if (!used)
                     {
                         this.UnusedItems.Add(item);
@@ -64,9 +69,34 @@ namespace robhabraken.SitecoreShrink
                         this.UnpublishedItems.Add(item);
                     }
 
-                    //TODO: find versions that are old and or obsolete
+                    if (this.HasMultipleVersions(item))
+                    {
+                        this.OldVersions.Add(item);
+                    }
                 }
             }
+        }
+        
+        /// <summary>
+        /// Checks if one or more language items of an item have more than one version.
+        /// </summary>
+        /// <remarks>
+        /// Note that it could be that an older verions is still in use due to custom publishing restrictions,
+        /// but checking that at this stage would be to costly, so we assume this items is an item to check when deleting old versions.
+        /// </remarks>
+        /// <param name="item">The item to check for multiple versions.</param>
+        /// <returns>True if one or more language items have multiple verions.</returns>
+        private bool HasMultipleVersions(Item item)
+        {
+            foreach (var language in item.Languages)
+            {
+                var languageItem = database.GetItem(item.ID, language);
+                if (languageItem.Versions.Count > 1)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public int MediaItemCount { get; set; }
@@ -74,9 +104,8 @@ namespace robhabraken.SitecoreShrink
         public List<Item> UnusedItems { get; set; }
 
         public List<Item> UnpublishedItems { get; set;  }
-
-        //TODO: fill this list
-        public List<Version> OldVersions { get; set; }
+        
+        public List<Item> OldVersions { get; set; }
 
     }
 }
