a68f08282bd506fd8be5749610dc8d4c310be48f
Rob Habraken <roberthabraken@hotmail.com>
2016-10-16 20:38:36 +0200

    Change test page

--- Website/index.aspx.cs
+++ Website/index.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 using robhabraken.SitecoreShrink;
 using Sitecore.Configuration;
 using Sitecore.Data.Items;
+using robhabraken.SitecoreShrink.Analyzing;
 
 namespace shrink
 {
@@ -19,22 +20,18 @@ namespace shrink
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            var mediaItemUsage = new MediaItemUsage("master");
-            //var itemReport = mediaItemUsage.ScanMediaLibraryOptimized();
-
-            MediaItemReport itemReport = null; // not populated yet in this version
-            mediaItemUsage.ScanMediaLibraryOptimizedJob();
+            var mediaScanner = new MediaScanner("master");
+            mediaScanner.ScanMediaLibraryJob();
             Response.Write("Job started");
-            return;
 
-            foreach (var item in itemReport.UnusedItems)
-            {
-                Response.Write(item.ID + " " + item.Name + "<br/>");
-            }
-            Response.Write("total: " + itemReport.MediaItemCount.ToString() + "<br />");
-            Response.Write("unused: " + itemReport.UnusedItems.Count.ToString() + "<br />");
-            Response.Write("unpublished: " + itemReport.UnpublishedItems.Count.ToString() + "<br />");
-            Response.Write("old versions: " + itemReport.OldVersions.Count.ToString() + "<br /><br />");
+            //foreach (var item in itemReport.UnusedItems)
+            //{
+            //    Response.Write(item.ID + " " + item.Name + "<br/>");
+            //}
+            //Response.Write("total: " + itemReport.MediaItemCount.ToString() + "<br />");
+            //Response.Write("unused: " + itemReport.UnusedItems.Count.ToString() + "<br />");
+            //Response.Write("unpublished: " + itemReport.UnpublishedItems.Count.ToString() + "<br />");
+            //Response.Write("old versions: " + itemReport.OldVersions.Count.ToString() + "<br /><br />");
 
             //var tidyJM = new TidyJobManager("master");
             //tidyJM.Download(itemReport.UnusedItems, @"D:\test\", false);
@@ -44,16 +41,16 @@ namespace shrink
             //tidyJM.Download();
             //tidyJM.Recycle();
 
-            var databaseHelper = new DatabaseHelper("master");
-            var dbReport = new DatabaseReport();
-            databaseHelper.GetSpaceUsed(ref dbReport);
-            Response.Write("database name: " + dbReport.DatabaseName + "<br />");
-            Response.Write("database size: " + dbReport.DatabaseSize + "<br />");
-            Response.Write("unallocated space: " + dbReport.UnallocatedSpace + "<br />");
-            Response.Write("reserved: " + dbReport.Reserved + "<br />");
-            Response.Write("data: " + dbReport.Data + "<br />");
-            Response.Write("index size: " + dbReport.IndexSize + "<br />");
-            Response.Write("unused: " + dbReport.UnusedData + "<br /><br />");
+            //var databaseHelper = new DatabaseHelper("master");
+            //var dbReport = new DatabaseReport();
+            //databaseHelper.GetSpaceUsed(ref dbReport);
+            //Response.Write("database name: " + dbReport.DatabaseName + "<br />");
+            //Response.Write("database size: " + dbReport.DatabaseSize + "<br />");
+            //Response.Write("unallocated space: " + dbReport.UnallocatedSpace + "<br />");
+            //Response.Write("reserved: " + dbReport.Reserved + "<br />");
+            //Response.Write("data: " + dbReport.Data + "<br />");
+            //Response.Write("index size: " + dbReport.IndexSize + "<br />");
+            //Response.Write("unused: " + dbReport.UnusedData + "<br /><br />");
 
             //databaseHelper.GetOrphanedBlobsSize(ref dbReport);
             //Response.Write("used blobs: " + dbReport.UsedBlobs + " MB<br />");
