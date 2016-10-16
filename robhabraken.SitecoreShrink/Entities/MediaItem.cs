cf6b4c5016411886ac9f6b16bd4985e953bef672
Rob Habraken <roberthabraken@hotmail.com>
2016-10-16 16:57:30 +0200

    Introducing new data model structure for reporting purposes...

---
+++ robhabraken.SitecoreShrink/Entities/MediaItem.cs
@@ -0,0 +1,37 @@
+﻿namespace robhabraken.SitecoreShrink.Entities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MediaItem
+    {
+        public MediaItem()
+        {
+            this.Name = string.Empty;
+            this.Children = new List<MediaItem>();
+            this.Size = 0;
+        }
+
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<MediaItem> Children { get; set; }
+
+        /// <summary>
+        /// Only applicable for media items, defaults to 0 when IsMediaFolder is true.
+        /// </summary>
+        public long Size { get; set; }
+        
+        public bool IsMediaFolder { get; set; }
+
+        public bool IsReferenced { get; set; }
+
+        /// <summary>
+        /// True if this item is published to at least one of the configured publishing targets.
+        /// </summary>
+        public bool IsPublished { get; set; }
+
+        public bool HasOldVersions { get; set; }
+    }
+}
