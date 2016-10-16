473cd2dc3066b7ddcfe6d9724c20b740dd91a2f5
Rob Habraken <roberthabraken@hotmail.com>
2016-10-16 20:35:17 +0200

    Rewrite structure and class composition of scanner and media item to report for more insight, better maintainable code and more functionality

---
+++ robhabraken.SitecoreShrink/Analyzing/MediaScanner.cs
@@ -0,0 +1,139 @@
+﻿namespace robhabraken.SitecoreShrink.Analyzing
+{
+    using Sitecore;
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Links;
+    using System;
+    using Sitecore.Jobs;
+    using System.Diagnostics;
+    using Entities;
+
+    /// <summary>
+    /// Utility class that is able to scan the media library to analyze item usage and space consumption. (RECOMPOSE THIS DESCRIPTION)
+    /// </summary>
+    public class MediaScanner
+    {
+        private Database database;
+        private MediaItemX mediaItem;
+
+        public MediaScanner(string databaseName)
+        {
+            this.database = Factory.GetDatabase(databaseName);
+        }
+
+        public void ScanMediaLibraryJob()
+        {
+            var action = "ScanMediaLibrary";
+            var jobName = string.Format("{0}_{1}", this.GetType(), action);
+
+            var jobOptions = new JobOptions(
+                jobName,
+                "Scanning media",
+                Context.Site.Name,
+                this,
+                action,
+                new object[0])
+            {
+                AfterLife = TimeSpan.FromMinutes(30)
+            };
+
+            var job = JobManager.Start(jobOptions);
+        }
+
+        public void ScanMediaLibrary()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            var root = database.Items["/sitecore/media library"];
+            this.mediaItem = new MediaItemX(root);
+
+            this.ScanItemsOf(root, this.mediaItem);
+
+            stopwatch.Stop();
+            var elapsedTime = stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.fffff");
+        }
+
+        private void ScanItemsOf(Item sitecoreItem, MediaItemX reportItem)
+        {
+            this.Analyze(sitecoreItem, reportItem);
+
+            if (Context.Job != null)
+            {
+                Context.Job.Status.Processed++;
+            }
+
+            if (sitecoreItem.HasChildren)
+            {
+                foreach (Item child in sitecoreItem.Children)
+                {
+                    var childItem = new MediaItemX(child);
+                    reportItem.Children.Add(childItem);
+
+                    this.ScanItemsOf(child, childItem);
+                }
+            }
+        }
+
+        private void Analyze(Item sitecoreItem, MediaItemX reportItem)
+        {
+            if (reportItem.IsMediaFolder.HasValue && !reportItem.IsMediaFolder.Value)
+            {
+                // update and get referrers
+                Globals.LinkDatabase.UpdateReferences(sitecoreItem);
+
+                ItemLink[] itemReferrers = null;
+                try
+                {
+                    itemReferrers = Globals.LinkDatabase.GetReferrers(sitecoreItem);
+
+                    // check validity of all referrers
+                    foreach (var itemLink in itemReferrers)
+                    {
+                        if (itemLink != null)
+                        {
+                            var referencedItem = itemLink.GetSourceItem();
+                            if (referencedItem != null)
+                            {
+                                reportItem.IsReferenced = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    //this.report(string.Format("Skipping this item because retrieving referrers failed due to {0}", exception.Message));
+                    return;
+                }
+
+                reportItem.IsPublished = new PublishingHelper().ListPublishedTargets(sitecoreItem).Count > 0;
+                reportItem.HasOldVersions = this.HasMultipleVersions(sitecoreItem);
+            }
+        }
+
+        /// <summary>
+        /// Checks if one or more language items of an item have more than one version.
+        /// </summary>
+        /// <remarks>
+        /// Note that it could be that an older verions is still in use due to custom publishing restrictions,
+        /// but checking that at this stage would be to costly, so we assume this items is an item to check when deleting old versions.
+        /// </remarks>
+        /// <param name="item">The item to check for multiple versions.</param>
+        /// <returns>True if one or more language items have multiple verions.</returns>
+        private bool HasMultipleVersions(Item item)
+        {
+            foreach (var language in item.Languages)
+            {
+                var languageItem = database.GetItem(item.ID, language);
+                if (languageItem.Versions.Count > 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
--- robhabraken.SitecoreShrink/Entities/MediaItemX.cs
+++ robhabraken.SitecoreShrink/Entities/MediaItemX.cs
@@ -1,15 +1,19 @@
 ﻿namespace robhabraken.SitecoreShrink.Entities
 {
+    using Sitecore.Data.Items;
     using System;
     using System.Collections.Generic;
+    using Analyzing;
 
-    public class MediaItem
+    public class MediaItemX
     {
-        public MediaItem()
+        public const string MEDIA_FOLDER_TEMPLATE_ID = "{FE5DD826-48C6-436D-B87A-7C4210C7413B}";
+
+        public MediaItemX()
         {
             this.ID = Guid.Empty;
             this.Name = string.Empty;
-            this.Children = new List<MediaItem>();
+            this.Children = new List<MediaItemX>();
             this.Size = 0;
             this.IsMediaFolder = null;
             this.IsReferenced = null;
@@ -17,11 +21,26 @@
             this.HasOldVersions = null;
         }
 
+        public MediaItemX(Item item)
+        {
+            this.ID = item.ID.Guid;
+            this.Name = item.Name;
+            this.Children = new List<MediaItemX>();
+
+            this.IsMediaFolder = item.Template.ID.ToString().Equals(MediaItemX.MEDIA_FOLDER_TEMPLATE_ID);
+
+            if (this.IsMediaFolder.HasValue && !this.IsMediaFolder.Value && item.Paths.IsMediaItem)
+            {
+                var mediaItem = (MediaItem)item;
+                this.Size = mediaItem.Size;
+            }
+        }
+
         public Guid ID { get; set; }
 
         public string Name { get; set; }
 
-        public List<MediaItem> Children { get; set; }
+        public List<MediaItemX> Children { get; set; }
 
         /// <summary>
         /// Only applicable for media items, defaults to 0 when IsMediaFolder is true.
