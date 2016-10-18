187374a00e9ce96afb31153409d6d54724f7f3c5
Rob Habraken <roberthabraken@hotmail.com>
2016-10-18 15:23:18 +0200

    Add skeleton of reporting class

--- robhabraken.SitecoreShrink/Analyzing/MediaReport.cs
+++ robhabraken.SitecoreShrink/Analyzing/MediaReport.cs
@@ -10,45 +10,74 @@
     public class MediaReport
     {
 
+        public List<MediaItemX> flatList;
+
+        public MediaReport(MediaItemX mediaItemX)
+        {
+
+        }
+
         /// <summary>
-        /// Count the number of media items in the itemReference object.
+        /// Returns the total number of media items in the media library, not including media folders.
         /// </summary>
-        /// <param name="itemReference"></param>
         /// <returns></returns>
-        public static int Count(MediaItemX itemReference)
+        public int MediaItemCount()
         {
-
-
             return 0;
         }
 
-        public static long TotalSize(MediaItemX itemRerence)
+        /// <summary>
+        /// Returns the total size of the media library in bytes, not including media folders.
+        /// </summary>
+        /// <returns></returns>
+        public long MediaLibrarySize()
         {
-
             return 0;
         }
 
-        public static long UnusedSize(MediaItemX itemRerence)
+        /// <summary>
+        /// Returns the number of media items that are referenced by other Sitecore items, not including media folders.
+        /// </summary>
+        /// <returns></returns>
+        public int ReferencedItemCount()
         {
-
             return 0;
         }
 
+        /// <summary>
+        /// Returns the total size of all referenced items in bytes, not including the media folders.
+        /// </summary>
+        /// <returns></returns>
+        public long ReferencedMediaSize()
+        {
+            return 0;
+        }
 
-        /**
-         * 
-         * 
-       * public int MediaItemCount { get; set; }
-       *
-      *  public long UnusedSize { get; set; }
-      *
-    *    public List<Item> UnusedItems { get; set; }
-    *
-     *   public List<Item> UnpublishedItems { get; set; }
-     *
-     *   public List<Item> OldVersions { get; set; }
-         * 
-         * 
-         */
+        /// <summary>
+        /// Returns the number of media items that are published to one or more publishing targets, not including media folders.
+        /// </summary>
+        /// <returns></returns>
+        public int PublishedItemCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the total size of all published items in bytes (to one or more publishing targets), not including media folders.
+        /// </summary>
+        /// <returns></returns>
+        public int PublishedMediaSize()
+        {
+            return 0;
+        }
+        
+        /// <summary>
+        /// Returns the number of media items that contain more than one version, not including media folders.
+        /// </summary>
+        /// <returns></returns>
+        public int OldVersionsItemCount()
+        {
+            return 0;
+        }
     }
 }
