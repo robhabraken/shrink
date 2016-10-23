4cfeebea1f97e12e06d1edf548e3e29fc06b6558
Rob Habraken <roberthabraken@hotmail.com>
2016-10-23 21:19:37 +0200

    Add demo custom SPEAK rendering

---
+++ robhabraken.SitecoreShrink/sitecore/Shell/client/YourApps/ViewSitecoreJobs/Renderings/SitecoreJobsListing.js
@@ -0,0 +1,15 @@
+﻿define(["sitecore"], function (Sitecore) {
+  var model = Sitecore.Definitions.Models.ControlModel.extend({
+    initialize: function (options) {
+      this._super();
+    }
+  });
+
+  var view = Sitecore.Definitions.Views.ControlView.extend({
+    initialize: function (options) {
+      this._super();
+    }
+  });
+
+  Sitecore.Factories.createComponent("SitecoreJobsListing", model, view, ".sc-SitecoreJobsListing");
+});
