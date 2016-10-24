a5ac8a74ece0775608c36e9c206070aee61c0c87
Rob Habraken <roberthabraken@hotmail.com>
2016-10-24 21:11:11 +0200

    Move functions to js file step 1

--- Website/sitecore modules/Shell/robhabraken/SitecoreShrink/shrink.js
+++ Website/sitecore modules/Shell/robhabraken/SitecoreShrink/shrink.js
@@ -1,5 +1,5 @@
 
-// Format the size of the element from bytes to a formatted and human readable file size string.
+// Format the size of an item or a category from bytes to a formatted and human readable file size string.
 function toReadableFileSizeString(fileSizeInBytes) {
 
     var i = -1;
@@ -11,3 +11,15 @@ function toReadableFileSizeString(fileSizeInBytes) {
 
     return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
 };
+
+// Sunburst chart helper function: when zooming, interpolate the scales.
+function arcTweenZoom(d) {
+  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+      yd = d3.interpolate(y.domain(), [d.y, 1]),
+      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+  return function(d, i) {
+    return i
+        ? function(t) { return arc(d); }
+        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+  };
+}
