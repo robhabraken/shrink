ca517128b4e8a21665c2796c42a20345c7649c1b
Rob Habraken <roberthabraken@hotmail.com>
2016-10-27 23:25:35 +0200

    Update Web API skeleton with action for each button

---
+++ robhabraken.SitecoreShrink/Controllers/MediaDashboardController.cs
@@ -0,0 +1,49 @@
+﻿namespace robhabraken.SitecoreShrink.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MediaDashboardController : Controller
+    {
+        public ActionResult ScanMedia(string name)
+        {
+            name += " Scanning!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ArchiveMedia(string name)
+        {
+            name += " Archiving!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult RecycleMedia(string name)
+        {
+            name += " Recycling!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteMedia(string name)
+        {
+            name += " Deleting!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DownloadMedia(string name)
+        {
+            name += " Downloading!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteOldVersions(string name)
+        {
+            name += " Deleting old versions!";
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
